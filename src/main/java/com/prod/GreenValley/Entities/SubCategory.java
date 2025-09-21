0b6b2c2baed1a96787f05af901228695fae3b5ec
subendu12345 <subendumal94@gamil.com>
2025-09-21 19:19:50 +0530

    fixed duplicate

--- src/main/java/com/prod/GreenValley/Entities/SubCategory.java
+++ src/main/java/com/prod/GreenValley/Entities/SubCategory.java
@@ -13,7 +13,7 @@ public class SubCategory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
 
     @Column(nullable = false, unique = true)
