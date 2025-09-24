5812b488c9b2b6cfefa04a51da4d06448bf0e478
subendu12345 <subendumal94@gamil.com>
2025-09-24 09:57:50 +0530

    fixed logic for trans report

--- src/main/java/com/prod/GreenValley/repository/TransactionRepo.java
+++ src/main/java/com/prod/GreenValley/repository/TransactionRepo.java
@@ -32,12 +32,13 @@ public interface TransactionRepo extends JpaRepository<Transction, Long>{
     List<CounterStockInDetail> getTotalTransctionVolume();
 
     String GET_TRANS_REPORT = """
-        SELECT t.transction_date, product_name, size, quantity, price, sum(size * quantity) as totaVolumeSale, SUM(price*quantity) as totalSalePrice  
+        SELECT t.transction_date, product_name, size, quantity, price, (size * quantity) as totaVolumeSale, (price*quantity) as totalSalePrice  
         FROM transction_item ti inner join transction as t on t.id = ti.transction_id
         WHERE is_extra_item = false
         AND t.transction_date >= :startDate
         AND t.transction_date <= :endDate
-        group by t.transction_date, product_name, size, quantity, price;
+        group by t.transction_date, product_name, size, quantity, price
+        order by product_name desc;
             """;
 
     @Query(value = GET_TRANS_REPORT, nativeQuery = true)
